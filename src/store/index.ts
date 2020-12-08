1c62a5b64d9c9545c87f5e17dd86da9137b12508
zhangping <zhangping@vip56.cn>
2020-12-08 15:50:02 +0800

    init

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+import {Public} from './modules/public';
+export default createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    Public
+  }
+})
